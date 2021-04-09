5899c2cdd16abc5e58a55802d498f75fdceac17b
Yoshi Yamaguchi <145104+ymotongpoo@users.noreply.github.com>
2021-04-09 17:15:58 +0900

    Fix to generate Ansible inventory file via Terraform

--- deploy.tf
+++ deploy.tf
@@ -38,14 +38,43 @@ variable "gce_ssh_user" {
 
 variable "gce_ssh_pub_key_file" {
     type = string
-    description = "path the public key file"
+    description = "path to the public key file"
     default = "~/.ssh/gcp_terraform.pub"
 }
+
+variable "gcp_service_account_file" {
+    type = string
+    description = "path to service account credential file"
+    default = "~/.credentials/yoshifumi-cloud-demo.json"
+}
+
 
 provider "google" {
     project = var.project_id
     zone = var.zone
     region = var.region
+}
+
+locals {
+    ansible_inventory = <<-EOT
+        ---
+        plugin: google.cloud.gcp_compute
+        auth_kind: serviceaccount
+        service_account_file: "${var.gcp_service_account_file}"
+        zones:
+        - "${var.zone}"
+        projects:
+        - "${var.project_id}"
+        hostnames:
+        - 'name'
+        compose:
+          ansible_host: networkInterfaces[0].accessConfigs[0].natIP
+    EOT
+}
+
+resource "local_file" "ansible_inventory_file" {
+    filename = "inventory.gcp.yaml"
+    content = local.ansible_inventory
 }
 
 resource "google_compute_instance" "shakesapp" {
@@ -54,6 +83,9 @@ resource "google_compute_instance" "shakesapp" {
     zone = var.zone
     tags = ["shakesapp-demo"]
 
+    depends_on = [
+        local_file.ansible_inventory_file,
+    ]
     boot_disk {
         initialize_params {
             image = "debian-cloud/debian-10"
@@ -80,6 +112,16 @@ resource "google_compute_instance" "shakesapp" {
     metadata = {
         block-project-ssh-keys = "true"
         ssh-keys = "${var.gce_ssh_user}:${file(var.gce_ssh_pub_key_file)}"
+    }
+
+    provisioner "remote-exec" {
+        inline = ["echo hello"]
+        connection {
+            type = "ssh"
+            host = self.network_interface[0].access_config[0].nat_ip
+            user = var.gce_ssh_user
+            private_key = file("~/.ssh/gcp_terraform")
+        }
     }
 
     provisioner "local-exec" {
